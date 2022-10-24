c533de23e013c0e99d6cc6f48cb08eb803342249
Maruko-coding <markojau@gmail.com>
2022-10-24 14:20:56 -0300

    deploy scripts

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -62,7 +62,7 @@ contract Lottery is VRFConsumerBaseV2, KeeperCompatibleInterface {
   // Functions
   // --------------------------------------------------
   constructor(
-    address vrfCoordinatorV2,
+    address vrfCoordinatorV2, // contract
     uint256 entranceFee,
     bytes32 gasLane,
     uint64 subscriptionId,
---
+++ deploy/01-deploy-lottery.js
@@ -0,0 +1,52 @@
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  const chainId = network.config.chainId
+  let vrfCoodrinatorV2Address, subscriptionId
+
+  if (developmentChains.includes(network.name)) {
+    const vrfCoodrinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+    vrfCoodrinatorV2Address = vrfCoodrinatorV2Mock.address
+    const transactionResponse = await vrfCoodrinatorV2Mock.createSubscription()
+    const transactionReceipt = await transactionResponse.wait(1)
+    subscriptionId = transactionReceipt.events[0].args.subId
+    // Fund subscription
+    await vrfCoodrinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
+  } else {
+    vrfCoodrinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"]
+    subscriptionId = networkConfig[chainId]["subscriptionId"]
+  }
+
+  const entranceFee = networkConfig[chainId]["entranceFee"]
+  const gasLane = networkConfig[chainId]["gasLane"]
+  const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"]
+  const interval = networkConfig[chainId]["interval"]
+  const args = [
+    vrfCoodrinatorV2Address,
+    entranceFee,
+    gasLane,
+    subscriptionId,
+    callbackGasLimit,
+    interval,
+  ]
+  const lottery = await deploy("Lottery", {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  })
+
+  if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    log("Verifying...")
+    await verify(lottery.address, args)
+  }
+  console.log("-----------------------------------")
+}
+
+module.exports.tags = ["all", "lottery"]
